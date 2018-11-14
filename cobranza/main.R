e81aad408ee35d4dab436c47ca9aa1cc49315706
Diego Osorio <diegoso-0989@hotmail.com>
2018-11-13 21:14:19 -0500

    Add files via upload

--- cobranza/main.R
+++ cobranza/main.R
@@ -3,6 +3,12 @@ library(DataExplorer)
 
 setwd("c:/users/rojo/desktop/idata/idata/cobranza")
 data <- fread("BaseCobranzas.csv")
-create_report(data)
 
+create_report(data) 
+### unique(data$Cupos)
 
+sum(is.na(data$`MORAMAX 18 meses`))
+
+tmp <- data[`Mora Inicial` <= "30",]
+
+summary(data$`Mora Inicial`)
